506d9175bf239ac067e351d6803c9217c74b6e09
wudm <977657115@qq.com>
2023-07-19 10:26:38 +0800

    2

--- app/src/main/java/com/example/recharge/MainActivity.kt
+++ app/src/main/java/com/example/recharge/MainActivity.kt
@@ -10,37 +10,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.recharge.databinding.ActivityMainBinding
 import com.example.recharge.ui.theme.RechargeTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            RechargeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RechargeTheme {
-        Greeting("Android")
-    }
-}
